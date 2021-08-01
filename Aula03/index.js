ad3b6c5594e02841d26aeb7b9ef140e9dcd8ce87
DavidBarbosa425 <davidbarbosa425@gmail.com>
2021-08-01 15:05:05 -0300

    Como modificar o valor de uma variavel

--- Aula03/index.js
+++ Aula03/index.js
@@ -1,1 +1,20 @@
-console.log(`Meu nome é "DAVID". Estou aprendendo Javascript às ${10} da manhã.`)
+// Exercício, modificar o valor das variaveis como descrito abaixo.
+
+let varA = 'A'; // B
+let varB = 'B'; // C
+let varC = 'C'; // A
+
+/*const varAtemp = varA; Opção criando mais uma variavel "varAtemp".
+ *varA = varB;           Dessa forma a varial deixa de ter o valor 
+ *varB = varC;           inicial e recebe uma copia do valor atribuido.
+ *varC = varAtemp;
+ *console.log(varA, varB, varC);
+ */
+
+
+[varA, varB, varC] = [varB, varC, varA]; 
+/* Opção sem criar uma nova variavel, 
+dessa forma as variaveis apontam para o valor na memoria sem alterar o valor inicial da variavel.
+*/
+
+console.log(varA, varB, varC);
