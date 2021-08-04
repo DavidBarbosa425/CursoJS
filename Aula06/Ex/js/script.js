fd23d424bbc0531ccd0260d06198653f6b46e818
DavidBarbosa425 <davidbarbosa425@gmail.com>
2021-08-03 23:43:13 -0300

    Exercícios com Numbers

--- Aula06/Ex/js/script.js
+++ Aula06/Ex/js/script.js
@@ -1,4 +1,8 @@
-let num = prompt('Digite um número: ');
+let num = Number(prompt('Digite um número: '));
+/**
+ * Todo valor retornado pelo prompt() vem como string 
+ * com o Number() é possível fazer a conversão.
+ */
 
 let spanEl = document.querySelector('#title');
 
@@ -6,4 +10,9 @@ spanEl.innerHTML = num;
 
 let divEl = document.querySelector('#text');
 
-divEl.innerHTML  = `Raiz quadrada: ${(num / 0.5) **9}`;
+divEl.innerHTML  = `<p>Raiz quadrada: ${num ** 0.5}</p>`;
+divEl.innerHTML += `<p>${num} é inteiro: ${Number.isInteger(num)}</p>`;
+divEl.innerHTML += `<p>${num} é NaN: ${Number.isNaN(num)}</p>`;
+divEl.innerHTML += `<p>Arredondado para baixo: ${Math.floor(num)}</p>`;
+divEl.innerHTML += `<p>Arredondado para cima: ${Math.ceil(num)}</p>`;
+divEl.innerHTML += `<p>Com duas casas decimais: ${num.toFixed(2)}</p>`
