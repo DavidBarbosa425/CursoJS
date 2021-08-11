61adce11c3cb24392e031d0ab1ae815ae23678fc
DavidBarbosa425 <davidbarbosa425@gmail.com>
2021-08-11 14:37:43 -0300

    for of

---
+++ Aula017/index.js
@@ -0,0 +1,37 @@
+const nome = 'David Barbosa';
+
+// for (let i = 0; i < nome.length; i++) {
+//     console.log(nome[i])
+// }
+
+// for (let i in nome) {
+//     console.log(nome[i])
+// }
+
+// for (let ivalor of nome) {
+//     console.log(ivalor)
+// }
+// D
+// a
+// v
+// i
+// d
+ 
+// B
+// a
+// r
+// b
+// o
+// s
+// a
+
+
+const nomes = ['David', 'João', 'Pedro', 'Antonio'];
+
+for (let valor of nomes) {
+    console.log(valor)
+}
+// David
+// João
+// Pedro
+// Antonio
