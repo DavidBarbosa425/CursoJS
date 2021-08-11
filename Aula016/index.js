5bdaff7e17ec5885a58a3520bfd09e0fb898f327
DavidBarbosa425 <davidbarbosa425@gmail.com>
2021-08-11 14:03:32 -0300

    for in

---
+++ Aula016/index.js
@@ -0,0 +1,28 @@
+// const frutas = ['pera', 'maça', 'salada mista'];
+
+// for (let i = 0; i < frutas.length; i++) {
+//     console.log(frutas[i]);
+// }
+// pera
+// maça
+// salada mista
+
+// for (let i in frutas) {
+//     console.log(frutas[i]);
+// }
+// pera
+// maça
+// salada mista
+
+// const discografia = {
+//     artista: 'Guns and Roses',
+//     album: 'appetite for destruction',
+//     ano: 1987
+// }
+
+// for (let key in discografia){
+//     console.log(discografia[key])
+//     Guns and Roses
+//     appetite for destruction
+//     1987
+// }
