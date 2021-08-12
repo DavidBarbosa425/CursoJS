b4e7d679976efad5b6229e7077ea4128cf8abea1
DavidBarbosa425 <davidbarbosa425@gmail.com>
2021-08-11 21:53:31 -0300

    Exercício com Lógica de programação

---
+++ Aula021/index.js
@@ -0,0 +1,11 @@
+let ePaisagem = (l , a) => l > a ? true : false;
+
+let resEpaisagem = ePaisagem(1080, 1920);
+console.log(resEpaisagem)
+
+// Solução com melhoria de código
+
+let ePaisagem2 = (l2, a2) => l2 > a2 ;
+
+let resEpaisagem2 = ePaisagem2(1920, 1080);
+console.log(resEpaisagem2)
