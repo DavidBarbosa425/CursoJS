57f863c9df5090bdcba56d14177847ae4f3999e2
DavidBarbosa425 <davidbarbosa425@gmail.com>
2021-08-19 22:47:25 -0300

    Concatenando arrays e ...spred

---
+++ Aula031/index.js
@@ -0,0 +1,9 @@
+let a1 = [1, 2, 3];
+let a2 = [4, 5, 6];
+//let a3 = a1.concat(a2, [7, 8, 9], 'David');
+//console.log(a3) // [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 'David' ]
+
+
+// let a3 = [...a1,...a2,...[7, 8, 9], 'David']; 
+// ...spred é a forma mais usada
+// console.log(a3) [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 'David' ]
