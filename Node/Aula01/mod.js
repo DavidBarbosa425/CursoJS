3d4865b5d6f79d59241b521cc7e525cb3c7de4dd
DavidBarbosa425 <davidbarbosa425@gmail.com>
2021-09-10 19:24:57 -0300

    modulo

---
+++ Node/Aula01/mod.js
@@ -0,0 +1,10 @@
+const nome = 'David';
+const sobrenome = 'Barbosa';
+
+const falaNome = () => nome + ' ' + sobrenome;
+
+exports.nome = nome;
+exports.sobrenome = sobrenome;
+exports.falaNome = falaNome;
+this.qualquerCoisa = 'O que eu quiser exportar.'
+// console.log(exports);
